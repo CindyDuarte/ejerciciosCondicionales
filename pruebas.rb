f8c6311b2de01fa0a12128b2457fd16123b79d68
Angie <arco527@gmail.com>
2019-03-04 17:42:31 -0500

    Conditionals exercises

---
+++ pruebas.rb
@@ -0,0 +1,29 @@
+#CONDICIONALES
+
+#Saluda a través de la consola! ej: "Hola makers, mi nombre es:-----"
+
+#1. A través de la consola es posible pedirle al usuario que ingrese su nombre, si es su nombre debe decir "hola (tuNombre)", de lo contrario, "No eres (nombre), intenta de nuevo"
+
+#2.Si el número esta entre 10 y 15 imprimir "El número esta entre 10 y 15", de lo contario "Intenta de nuevo"
+
+#3. A través de la consola es posible pedirle al usuario que ingrese su Color favorito, si esta entre rojo y verde, debe decir "Excelente Color", de lo contrario "Intenta de nuevo"
+
+#OPERADOR TERNARIO
+#A travéz de un operador ternario escribe si Alejandro hoy llego tarde a clase, si es cierto escribe "Angie lo sabia", de lo contrario, "Él no llego tarde"
+
+# puts "llego tarde?"
+# name = "Alejandro"
+
+#A travéz de un operador ternario escribe si el número que ingresas "es el correcto", de lo contrario escribe "Intenta de nuevo" (arregla el código si hace falta)
+
+# puts "Ingresa un número de 1 al 5"
+# num = gets.chomp
+
+#each
+# Escribe una solución donde se listen todos los compañeros del salón
+
+# Escribe una solución donde se listen los números del 1 al 20
+# Escribe una solución donde se listen los números del 20 al 1
+
+#times
+#Haz un triangúlo rectángulo
